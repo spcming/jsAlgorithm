dceec8f592bf8c706ad25c4b0b4b23474771a166
MING0604 <1206710179@qq.com>
2020-09-28 18:57:32 +0800

    2020/9/28

---
+++ 7. 整数反转.js
@@ -0,0 +1,23 @@
+// 给出一个 32 位的有符号整数，你需要将这个整数中每位上的数字进行反转。
+
+// 示例 1:
+
+// 输入: 123
+// 输出: 321
+//  示例 2:
+
+// 输入: -123
+// 输出: -321
+// 示例 3:
+
+// 输入: 120
+// 输出: 21
+
+var reverse = function(x) {
+    let result = parseInt(Math.abs(x).toString().split('').reverse().join(''))
+    if(x<0) result = -result;
+    if(result<-(2**31)||result>(2**31-1)){
+        return 0
+    }
+    return result
+};
