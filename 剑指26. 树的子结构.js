248c15c43bf56ec3118ce1ff921c609a3cac29f6
MING0604 <1206710179@qq.com>
2020-10-07 17:39:00 +0800

    前序遍历+两节点是否有包含关系

--- 剑指26. 树的子结构.js
+++ 剑指26. 树的子结构.js
@@ -11,13 +11,20 @@
  * @return {boolean}
  */
 var isSubStructure = function(A, B) {
+    if(!B){
+        return false;
+    }
     let res = false;
     // 判断在根节点相同时，A节点是否包含B节点
     function isSame(A,B){
-        let sameRoot = (A.val === B.val);
-        let sameLeft = B.left == null || isSame(A.left,B.left);
-        let sameRight = B.right == null || isSame(A.right,B.right);
-        return sameRoot && sameLeft && sameRight;
+        if(A){
+            let sameRoot = (A.val === B.val);
+            let sameLeft = B.left == null || isSame(A.left,B.left);
+            let sameRight = B.right == null || isSame(A.right,B.right);
+            return sameRoot && sameLeft && sameRight;
+        }else{
+            return false
+        }
     }
 
     function readTree(a,func){
@@ -37,17 +44,3 @@ var isSubStructure = function(A, B) {
     })
     return res
 };
-function TreeNode(val) {
-    this.val = val;
-    this.left = this.right = null;
-}
-let A = new TreeNode(6)
-A.left = new TreeNode(4)
-A.right = new TreeNode(5)
-A.left.left = new TreeNode(1)
-A.left.right = new TreeNode(2)
-
-let B = new TreeNode(1)
-// B.left = new TreeNode(1)
-
-console.log(isSubStructure(A,B))
