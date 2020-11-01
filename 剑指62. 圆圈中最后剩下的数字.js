91776193fd96a49e264f036c98fbcc26f8a09454
MING0604 <1206710179@qq.com>
2020-11-01 14:20:56 +0800

    约瑟夫环

--- 剑指62. 圆圈中最后剩下的数字.js
+++ 剑指62. 圆圈中最后剩下的数字.js
@@ -4,26 +4,10 @@
  * @return {number}
  */
 var lastRemaining = function(n, m) {
-    let arr = []
-    for(let i = 0; i < n; i++){
-        arr[i] = i
+    let index = 0;
+    for(let i = 0; i < n - 1; i++){
+        index = (index + m)%(2+i)
     }
-    let index = 0,
-        num = 1;
-    while(arr.length!=1){
-        if(index == arr.length){
-            index = 0
-        }
-        
-        if(num%m === 0){
-            arr.splice(index,1)
-            num = 1
-        }else{
-            index++
-            num++
-        }
-        
-    }
-    return arr[0]
+    return index
 };
-lastRemaining(5,3)
+console.log(lastRemaining(5,3))
