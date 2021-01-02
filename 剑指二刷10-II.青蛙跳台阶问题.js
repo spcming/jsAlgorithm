8d63dc9565ccf16c5d19ee9fb29118b7d2dde2da
MING0604 <1206710179@qq.com>
2021-01-02 10:14:51 +0800

    类斐波那契数列，动态规划

---
+++ 剑指二刷10-II.青蛙跳台阶问题.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var numWays = function(n) {
+    if(n <= 1){
+        return 1
+    }
+    let a = 2, b = 1;
+    for(let i = 0; i < n - 2; i++){
+        [a, b] = [a + b, a]
+        a = a % 1000000007
+        b = b % 1000000007
+    }  
+    return a
+};
