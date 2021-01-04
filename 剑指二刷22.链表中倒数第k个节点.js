4ceb4a86c97fcd79628d12706f7825f4b9811bd1
MING0604 <1206710179@qq.com>
2021-01-04 10:58:37 +0800

    双指针

---
+++ 剑指二刷22.链表中倒数第k个节点.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} k
+ * @return {ListNode}
+ */
+var getKthFromEnd = function(head, k) {
+    let pre = head,cur = head;
+    for(let i = 0; i < k - 1; i++){
+        cur = cur.next;
+    }
+    while(cur.next!=null){
+        cur = cur.next
+        pre = pre.next
+    }
+    return pre
+};
