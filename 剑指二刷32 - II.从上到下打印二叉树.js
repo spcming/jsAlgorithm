54fb1f4a49571baaf231ae90c43243bdd81a8831
MING0604 <1206710179@qq.com>
2021-01-07 12:08:15 +0800

    bfs

---
+++ 剑指二刷32 - II.从上到下打印二叉树.js
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrder = function(root) {
+    if(!root){
+        return []
+    }
+    let queue = []
+    queue.push(root)
+    let i = 1,
+        queLen = 1,
+        j = 1,
+        res = []
+    res[j-1] = new Array()
+    while(queue.length){
+        let cur = queue.shift()
+        res[j-1].push(cur.val)
+        cur.left && queue.push(cur.left)
+        cur.right && queue.push(cur.right)
+        if(i === queLen){
+            queLen = queue.length
+            i=1
+            j++
+            res[j-1] = new Array()
+        }else{
+            i++
+        }
+    }
+    res.pop()
+    return res
+};
