ab08eae7075850eacdd92cb13d04613a105c328e
MING0604 <1206710179@qq.com>
2021-01-10 10:45:55 +0800

    动规

---
+++ 剑指二刷38.字符串的排列.js
@@ -0,0 +1,33 @@
+/**
+ * @param {string} s
+ * @return {string[]}
+ */
+var permutation = function(s) {
+    function dp(char, arr){
+        let length = arr.length
+        let resArr = []
+        for(let i = 0; i < length; i++){
+            let arrLen = arr[0].length
+            // console.log(arrLen)
+            for(let j = 0; j <= arrLen; j++){
+                let item = arr[i].slice(0,j)+char+arr[i].slice(j)
+                if(map[item]){
+                    continue
+                }
+                resArr.push(item)
+                map[item] = true
+            }
+        }
+        return resArr
+    }
+    let res = []
+    let map = {}
+    if(!s.length){
+        return []
+    }
+    res.push(s[0])
+    for(let i = 1; i < s.length; i++){
+        res = dp(s[i],res)
+    }
+    return res
+};
