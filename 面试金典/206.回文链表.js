b6bd439e02a71a40b60c1513b34169e281674d7d
spcming <spcming@163.com>
2022-03-03 09:50:16 +0800

    翻转链表 & 块慢指针

--- 面试金典/206.回文链表.js
+++ 面试金典/206.回文链表.js
@@ -11,3 +11,57 @@
 
 // 输入： 1->2->2->1
 // 输出： true 
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+ var isPalindrome = function(head) {
+    let p1 = head, p2 = head;
+    if(!p2) return true
+    while(p2.next){
+        p2 = p2.next
+        if(p2.next){
+            p2 = p2.next
+        }
+        p1 = p1.next; // p1为后半段开头, 前半段小于等于后半段
+    }
+    let revert = null, endFlag = p1;
+    if(!p1.next){
+        revert = p1;
+    }else{
+        let p2 = p1.next;
+        if(!p2.next){
+            p2.next = p1;
+            p1.next = null;
+            revert = p2;
+        }else{
+            let p3 = p2.next;
+            while(p3){
+                p2.next = p1;
+                p1 = p2;
+                p2 = p3;
+                p3 = p3.next;
+            }
+            p2.next = p1;
+            revert = p2;
+        }
+    }
+    let p = head;
+    while(p !== endFlag){
+        if(p.val === revert.val){
+            p = p.next;
+            revert = revert.next
+        }else{
+            return false
+        }
+    }
+    return true
+
+};
